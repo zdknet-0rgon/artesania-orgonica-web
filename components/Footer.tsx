b38fae7a25375880b1765ca62b0d9f0c72575982
zdknet-0rgon <zdknet@gmail.com>
2025-11-09 13:59:13 +0100

    Crear Footer.tsx faltante para resolver error 'Module not found' en layout.tsx.

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{padding:'24px', background:'#222', color:'#ddd', textAlign:'center'}}>
+      Artesanía Orgónica © 2025
+    </footer>
+  );
+}
