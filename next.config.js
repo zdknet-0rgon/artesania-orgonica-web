b8978df20a150b6a7d05ae3f07a499b7992d4fa6
zdknet-0rgon <zdknet@gmail.com>
2025-11-14 00:00:19 +0100

    fix: Añade resolución de alias '@' en Webpack/Next.js para solución de imports críticos y header en producción (automatización La Bestia)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   images: {
     domains: [],
     formats: ['image/avif', 'image/webp'],
   },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    return config;
+  },
 }
-
-module.exports = nextConfig
+module.exports = nextConfig;
