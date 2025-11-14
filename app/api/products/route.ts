a5729d39d70797b17ae7352fe6f2cbf9568d0874
zdknet-0rgon <zdknet@gmail.com>
2025-11-14 23:18:51 +0100

    Corrijo import de products.json usando alias correcto en route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import productsData from '@/app/data/products.json';
+import productsData from '@/data/products.json';
 
 export async function GET() {
   return NextResponse.json(productsData);
